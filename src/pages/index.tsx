28c174b13fd42633006c782bda0b6735e37c0a29
Plinio Araujo <plinioglad@gmail.com>
2022-02-19 16:56:28 -0300

    Next.js - Componente Tabela #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 20, '2'),
+    new Cliente('Carlos', 45, '3'),
+    new Cliente('José', 55, '4'),
+    new Cliente('Alberto', 25, '5'),
+    new Cliente('Amelia', 22, '6'),
+    new Cliente('Vanessa', 30, '7'),
+    new Cliente('Adriana', 47, '8'),
+  ]
+  
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +22,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>        
+        <Tabela clientes={clientes}></Tabela>     
       </Layout>
     </div>
   )
